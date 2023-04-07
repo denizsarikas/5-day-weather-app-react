b5710f5cce305d3ca2c5834b139606672bc9ba00
Deniz Sarıkaş <75947870+denizsarikas@users.noreply.github.com>
2023-04-07 15:52:59 +0300

    fully functional
    +Responsive design
    +Styles
    +Different Background each refresh
    -Background images will be in context and change due to different city input

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-//components
 import Header from "../src/components/Header"
 import Main from "../src/components/Main"
 import Footer from "../src/components/Footer"
 
+// use require.context to import all background images in the assets folder
+const backgroundImages = require.context('./assets', true, /\.(jpg|jpeg|png)$/);
+// create an array of all image filenames
+const backgroundImageFilenames = [...Array(33).keys()].map(i => `${i + 1}.jpg`);
+//console.log(backgroundImageFilenames)
+
 function App() {
+  // randomly select a background image filename from the array
+  const randomBackgroundImageFilename = backgroundImageFilenames[Math.floor(Math.random() * backgroundImageFilenames.length)];
+  // get the selected background image using require.context
+  const backgroundImage = backgroundImages(`./${randomBackgroundImageFilename}`);
+
   return (
-    <div className="App"
-      style={{
-        backgroundImage: `url("../src/assets/1.jpg")`
-      }}
-      >
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+      <div className="App" style={{backgroundImage: `url(${backgroundImage})`}}>
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+
   );
 }
 
